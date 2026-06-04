a407be01e118a4fe0a5ef2aafd49be65f039a26b
Diego Queres <diegoqueres81@gmail.com>
2026-06-04 18:41:24 -0300

    New project files

---
+++ src/main/kotlin/com/algaworks/algasensors/temperature/processing/TemperatureProcessingApplication.kt
@@ -0,0 +1,11 @@
+package com.algaworks.algasensors.temperature.processing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TemperatureProcessingApplication
+
+fun main(args: Array<String>) {
+	runApplication<TemperatureProcessingApplication>(*args)
+}
